31cc71435593201d38060c993e60cbf8b1c5182a
Raymond Z <raykyri@gmail.com>
2025-09-18 17:34:52 -0400

    chore: remove buffer polyfills

--- vite.config.js
+++ vite.config.js
@@ -1,56 +1,7 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import inject from "@rollup/plugin-inject"
-import { fileURLToPath, URL } from "node:url"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    // Provide global Buffer and process polyfills in the browser
-    {
-      ...inject({
-        Buffer: ["buffer", "Buffer"],
-        process: "process",
-        // Don't inject into React to avoid breaking named exports
-        exclude: ["node_modules/react/**", "node_modules/react-dom/**"],
-      }),
-      apply: "build",
-    },
-  ],
-  resolve: {
-    alias: {
-      buffer: "buffer/",
-      process: "process/browser",
-      util: "util/",
-      crypto: "crypto-browserify",
-      stream: "stream-browserify",
-      assert: "assert",
-      events: "events/",
-      vm: "vm-browserify",
-    },
-  },
-  define: {
-    "process.env": {},
-  },
-  optimizeDeps: {
-    include: [
-      "buffer",
-      "process",
-      "util",
-      "crypto-browserify",
-      "stream-browserify",
-      "assert",
-      "events",
-      "vm-browserify",
-    ],
-    esbuildOptions: {
-      define: {
-        global: "globalThis",
-      },
-      inject: [
-        fileURLToPath(new URL("./src/vite-buffer-shim.ts", import.meta.url)),
-      ],
-    },
-  },
+  plugins: [react()],
 })
